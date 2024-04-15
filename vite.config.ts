edad4b324d030cedac23a458dbf8162a15dccf39
Mehedi Hasan <developerhasan99@gmail.com>
2024-04-15 09:27:27 +0600

    Added base in vite.congig.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
+	base: "./",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
